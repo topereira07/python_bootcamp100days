5e5b5557f030923affbd3e574febd8837d8dd74e
To Pereira <topereira07@gmail.com>
2024-10-03 16:31:55 +0100

    day 2 finished - Project Tip Calculator

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,17 @@
-len("Hello")
+#Subscripting
+print("Hello"[0])
+
+print("Hello"[-1]) # print last character of the string
+
+#Large Integers
+print(123_456_789) # replace the comas, ignoring
+
+#Float
+print(3.14159)
+
+#Boolean
+print(True)
+print(False)
+
+street_name = "Abbey Road"
+print(street_name[4] + street_name[7])
--- Day 2/Mathematical Operations/task.py
+++ Day 2/Mathematical Operations/task.py
@@ -1,1 +1,21 @@
+from lib2to3.pygram import python_grammar_no_print_statement
+
 print("My age: " + str(12))
+
+print (6 / 3) #Implicit type casting to float
+print(6 // 3) # not casting, but it rounds down always
+
+print(2 * 3)
+print(2**3) # 2 to the power of 3 so 2*2*2
+
+# PEMDASLR -  parenthesis ()
+#           exponents **
+#           multiplication * OR
+#           division /
+#           addition + OR
+#           subtraction -
+#           left to right
+
+print(3 / 3 * 3 + 3 - 3)
+
+print(3 * (3 + 3) / 3 - 3)
