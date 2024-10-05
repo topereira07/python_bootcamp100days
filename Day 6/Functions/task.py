2946e5adb3260238ebe4d1b0dda7b5cb1540a84d
To Pereira <topereira07@gmail.com>
2024-10-06 00:04:44 +0100

    day 6 finished - Project Escaping the Maze

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,11 @@
+print("hello") #function
+
+def my_function(): #defining the function
+    print("Hello")
+    print("Bye")
+
+
+my_function() #calling the function
+
+# played with reeborg.ca for functions learning
+# all challenges done
