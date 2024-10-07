7f80356fab7f6f62e176535fcd2159075114325a
To Pereira <topereira07@gmail.com>
2024-10-07 22:30:33 +0100

    day 4 finished - Project Caesar Cypher

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,8 @@
+def greet(name): # name is the parameter
+    print(f"Olá como estás {name}?")
+    print(f"Hi there, how are you {name}?")
+    print(f"Hola, que tal {name}?")
+
+name = input("What is your name?")
+
+greet(name) # name passed here is the argument
--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,32 @@
 # Functions with input
 
-def greet_with_name(name):
+def greet_with_name(name, location):
     print(f"Hello {name}")
     print(f"How do you do {name}?")
+    print(f"How is it there in {location}?")
+
+name = input("What is your name?")
+location = input("What is your current location?")
+greet_with_name(name, location)
+
+greet_with_name(location="Porto", name="Pereira")
 
 
-greet_with_name("Jack Bauer")
+def calculate_love_score(name1, name2):
+    total_letters_in_true = 0
+    total_letters_in_love = 0
+    full_name = name1 + name2
+    for letter in full_name:
+        if letter == "t" or letter == "r" or letter == "u" or letter == "e":
+            total_letters_in_true += 1
+        if letter == "l" or letter == "o" or letter == "v" or letter == "e":
+            total_letters_in_love += 1
+    love_score = str(total_letters_in_true)+str(total_letters_in_love)
+    print(love_score)
+
+
+
+name1 = "Kanye West"
+name2 = "Kim Kardashian"
+
+calculate_love_score(name1, name2)
